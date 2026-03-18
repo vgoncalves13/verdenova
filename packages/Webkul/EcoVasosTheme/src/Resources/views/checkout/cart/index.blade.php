27dc8ce84cddbcad0e7a02b2f5e159b42562fddd
Victor Hugo Alves <vgoncalves360@hotmail.com>
2026-03-18 01:46:21 -0300

    ajustada lógica de cálculo e aplicação de métodos de envio no tema EcoVasos, incluindo melhorias no formulário de estimativa de frete, suporte a seleção automática do método mais barato e ajustes visuais no componente de lista de métodos

--- packages/Webkul/EcoVasosTheme/src/Resources/views/checkout/cart/index.blade.php
+++ packages/Webkul/EcoVasosTheme/src/Resources/views/checkout/cart/index.blade.php
@@ -12,47 +12,18 @@
 @endPush
 
 <x-shop::layouts
-    :has-header="false"
+    :has-header="true"
     :has-feature="false"
-    :has-footer="false"
+    :has-footer="true"
 >
     <!-- Page Title -->
     <x-slot:title>
         @lang('shop::app.checkout.cart.index.cart')
     </x-slot>
 
-    <div class="flex min-h-screen flex-col bg-green-50">
+    <div class="bg-green-50">
 
     {!! view_render_event('bagisto.shop.checkout.cart.header.before') !!}
-
-    <!-- Page Header -->
-    <div class="flex flex-wrap bg-darkGreen shadow-md">
-        <div class="flex w-full justify-between px-[60px] py-4 max-lg:px-8 max-md:px-4">
-            <div class="flex items-center gap-x-14 max-[1180px]:gap-x-9">
-                {!! view_render_event('bagisto.shop.checkout.cart.logo.before') !!}
-
-                <a
-                    href="{{ route('shop.home.index') }}"
-                    class="flex min-h-[30px]"
-                    aria-label="@lang('shop::app.checkout.cart.index.bagisto')"
-                >
-                    <img
-                        src="{{ core()->getCurrentChannel()->logo_url ?? bagisto_asset('images/logo.svg') }}"
-                        alt="{{ config('app.name') }}"
-                        width="131"
-                        height="29"
-                    >
-                </a>
-
-                {!! view_render_event('bagisto.shop.checkout.cart.logo.after') !!}
-            </div>
-
-            @guest('customer')
-                @include('shop::checkout.login')
-            @endguest
-        </div>
-    </div>
-
     {!! view_render_event('bagisto.shop.checkout.cart.header.after') !!}
 
     <div class="flex-auto">
@@ -97,7 +68,7 @@
         {!! view_render_event('bagisto.shop.checkout.cart.cross_sell_carousel.after') !!}
     @endif
 
-    </div><!-- end bg-green-50 wrapper -->
+    </div>{{-- end bg-green-50 --}}
 
     @pushOnce('scripts')
         <script
